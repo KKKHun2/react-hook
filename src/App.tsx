fbaf5de507934cc60dfb5edc9431cc2e541ac2b8
KKKHun2 <1224kim2son@naver.com>
2023-12-09 16:31:35 +0900

    react memo, useCallback.useRef 등등 실행

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 
 function App() {
+const [number,setNumber] =useState(0)
+const countRef = useRef(0)
+
+const increaseCountNumber = useCallback(
+  () =>{
+    setNumber(number +1);
+  },[number]) 
+//ref는 재렌더링되야 값이 표시되고 재렌더링되도 값이 초기화되지않는다.
+const increaseCountRef = () =>{
+ countRef.current = countRef.current +1;
+}
+
+
+
   return (
     <div>
-
+      <input value={number } placeholder="숫자" />
+      <p>{number}</p>
+      <p>ref:{countRef.current}</p>
+      <button onClick={increaseCountNumber}>숫자올리기</button>
+      <button onClick={increaseCountRef}>ref올리기</button>
     </div>
   );
 }
